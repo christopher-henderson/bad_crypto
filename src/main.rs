00c80f0c49582da07993fbf2fb105f8c853831d9
christopher-henderson <chris@chenderson.org>
2018-07-05 18:51:46 -0700

    first drop

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::vec::Vec;
+
+extern crate time;
+extern crate bad_crypto;
+
+use bad_crypto::{encrypt, decrypt};
+
+
+const ORIGINAL: &str = "/Users/chris/Documents/personal/rust/projects/bad_crypto/ci_real_word_deployments.mov";
+const CIPHER: &str = "/Users/chris/Documents/personal/rust/projects/bad_crypto/out.mov";
+
+fn dec() {
+	let mut plaintext = fs::read(CIPHER).unwrap();
+    let key = 21234111;
+    let start = time::PreciseTime::now();
+	decrypt(&mut plaintext, key);
+	let end = time::PreciseTime::now();
+	println!("finished in {} seconds", start.to(end));
+	fs::write("/Users/chris/Documents/personal/rust/projects/bad_crypto/decrypted.mov", plaintext).unwrap();
+}
+
+fn enc() {
+	let mut plaintext = fs::read(ORIGINAL).unwrap();
+    let key = 21234111;
+    let start = time::PreciseTime::now();
+	encrypt(&mut plaintext, key);
+	let end = time::PreciseTime::now();
+	println!("finished in {} seconds", start.to(end));
+	fs::write("/Users/chris/Documents/personal/rust/projects/bad_crypto/out.mov", plaintext).unwrap();
+}
+
+fn main() {
+	enc();
+	dec();
+}
